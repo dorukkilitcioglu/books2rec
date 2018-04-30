31cf6d136f9843e456cbce43d7417d7ee87b320c
nickgreenquist <nickgreenquist@gmail.com>
2018-04-30 14:07:01 -0400

    generate sim matrices before so the web app can simply load them. Fixed some styling and response code in web app. Using larger svd and feature matrices for item matrix generation

--- WebApp/app.py
+++ WebApp/app.py
@@ -17,21 +17,20 @@ data_path = '../../goodbooks-10k/'
 bookid_to_title = {}
 title_to_bookid = {}
 item_matrix = None
-svd = None
+qi = None
 cosine_sim_item_matrix = None
 cosine_sim_feature_matrix = None
-cosine_sim_svd = None
 books = None
 titles = []
 error_message = "I can't seem to find anything with what you gave me, I'm sorry"
 
 def load_books():
     global books, titles
-
     books = get_book_dataframe(data_path)
     for index, row in books.iterrows():
         titles.append(row['title'])
     titles.sort()
+    print('books loaded')
 
 def load_books_mappers():
     global bookid_to_title, title_to_bookid
@@ -41,39 +40,29 @@ def load_books_mappers():
         for i, line in enumerate(reader):
             bookid_to_title[line[0]] = line[10]
             title_to_bookid[line[10]] = line[0]
+    print('books mapper loaded')
 
 def load_item_matrix():
     global item_matrix, cosine_sim_item_matrix, cosine_sim_feature_matrix
-
-    filename = '../.tmp/item_matrix.npy'
-    item_matrix = np.load(filename)
-    cosine_sim_item_matrix = cosine_similarity(item_matrix)
-
-    part = item_matrix[:,10:110] #change this depending on which part of item matrix is features
-    cosine_sim_feature_matrix = cosine_similarity(part)
+    item_matrix = np.load('../.tmp/item_matrix.npy')
+    print('item matrix loaded')
+    
+    cosine_sim_item_matrix = np.load('../.tmp/cosine_sim_item_matrix.npy')
+    cosine_sim_feature_matrix = np.load('../.tmp/cosine_sim_feature_matrix.npy')
+    print('similarity matrices loaded')
 
 def load_svd_matrix():
-    global svd, cosine_sim_svd
-
-    filename = '../.tmp/svd_50_10'
-    svd = dump.load(filename)[1]
-    cosine_sim_svd = cosine_similarity(svd.qi)
+    global qi
+    qi = np.load('../.tmp/svd_100_300.npy')
+    print('svd matrix loaded')
 
 @app.route('/')
 def my_form():
     global books, titles
-
     load_books_mappers()
-    print('book title mapper loaded')
-
     # load_svd_matrix()
-    # print('svd loaded')
-
     load_books()
-    print('books loaded')
-
     load_item_matrix()
-    print('item_matrix loaded')
     return render_template('book_list.html', titles=titles)
 
 @app.route('/', methods=['POST', 'POST2'])
@@ -85,26 +74,28 @@ def my_form_post():
 
         if text not in title_to_bookid:
             return render_template('book_list.html',
-                                    error=error_message,
+                                    response=error_message,
                                     titles=titles)
 
         recs = get_recommendations(books, bookid_to_title, title_to_bookid, text, [cosine_sim_item_matrix], [1])
         return render_template('book_list.html', 
                                 toPass=recs,
-                                titles=titles)
+                                titles=titles,
+                                response='Showing Recommendations for: ' + text)
     
     if 'book_similar' in request.form:
         text = request.form['books']
 
         if text not in title_to_bookid:
             return render_template('book_list.html',
-                                    error=error_message,
+                                    response=error_message,
                                     titles=titles)
 
         recs = get_recommendations(books, bookid_to_title, title_to_bookid, text, [cosine_sim_feature_matrix], [1])
         return render_template('book_list.html', 
                                 toPass=recs,
-                                titles=titles)
+                                titles=titles,
+                                response='Showing Similar Books to: ' + text)
 
     if 'user_recs' in request.form:
         text = request.form['text']
@@ -112,18 +103,19 @@ def my_form_post():
         q = get_user_vector(text)
         if q is None:
             return render_template('book_list.html',
-                                    error=error_message,
+                                    response=error_message,
                                     titles=titles)
 
         # Get recs using item_matrix
         top_books = get_top_n_recs(map_user(q, item_matrix), books, 30, q)
 
         # Get recs using svd
-        # top_books = get_top_n_recs(map_user(q, svd.qi), books, 30, q)
+        # top_books = get_top_n_recs(map_user(q, qi), books, 30, q)
 
         return render_template('book_list.html', 
                                 toPass=top_books,
-                                titles=titles)
+                                titles=titles,
+                                response='Showing Recommendations for: ' + text)
     else:
         return 'ERROR'
 
