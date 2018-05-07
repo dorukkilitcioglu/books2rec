aaef8e7cf8502e8b2c8b6aadc3c7005ca15fd0bf
panghalamit <panghalamit1892@gmail.com>
2018-05-07 11:56:04 -0400

    added code to parse book title and output recs for books-ids in input

--- Mahout/RecommenderSystem/src/bookrecommender/RecommendationMain.java
+++ Mahout/RecommenderSystem/src/bookrecommender/RecommendationMain.java
@@ -4,7 +4,9 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 import org.apache.mahout.cf.taste.common.TasteException;
@@ -37,19 +39,44 @@ public class RecommendationMain {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		BufferedReader br = null;
+		String st;
+	    StringTokenizer st1 = null;
+		
+		//READ From Book.csv for title
+		Map<Integer, String> iDtoTitle = new HashMap<Integer, String>();
+		br = new BufferedReader(new FileReader(new File(args[1])));
+		br.readLine(); //skip 1st line
+		String orig_title = null;
+		int id;
+		while ((st = br.readLine()) != null) {	
+	        st1 = new StringTokenizer(st, ",");
+		    id = Integer.parseInt(st1.nextToken()); 
+		    for(int i=0; i<8; i++)
+		    	st1.nextToken();//skip tokens
+	        orig_title = st1.nextToken();
+	        iDtoTitle.put(id, orig_title);
+		  }
+		br.close();
+		//READ List to books_id to recommend
+		List<Integer> book_id_input = new ArrayList<Integer>();
+		br = new BufferedReader(new FileReader(new File(args[2])));
+		while ((st = br.readLine()) != null) {	
+		   book_id_input.add(Integer.parseInt(st));
+		}
+		br.close();
+		
+		//Check if content-based
 		boolean content_based=false;
-		if(args.length==2)
+		if(args.length==4)
 			content_based=true;
 		ItemBasedRecommender recommender_item =null;
-		if(content_based){
-		
-		
+		if(content_based){		
 		//TODO: Mem optimization to load 1.2 GB 10k*10k Book Feature Similarity Matrix for Item Based Rec
 		List<List<Float>> features = new ArrayList<List<Float>>();
-		BufferedReader br = new BufferedReader(new FileReader(new File(args[1]))); // reads item-item content similarity file 
+		br = new BufferedReader(new FileReader(new File(args[3]))); // reads item-item content similarity file 
 		 
-		  String st;
-		  StringTokenizer st1 = null;
+		  
 		  List<Float> temp=null;
 		  while ((st = br.readLine()) != null) {
 			temp=new ArrayList<Float>();  		
@@ -60,19 +87,20 @@ public class RecommendationMain {
 	        }
 	        features.add(temp);
 		  }
+		  br.close();
 		  ItemSimilarity book_similarity = new BookSimilarity(features);
 		  ItemSimilarity item_similarity = new GenericItemSimilarity(book_similarity,model, 100);
 		  recommender_item = new GenericItemBasedRecommender(model, book_similarity);
 		  
 		}
 		
-		/*
-		 //Item Based CF 
-		 ItemSimilarity itemSimilarity = new LogLikelihoodSimilarity(dataModel);
-		//Create an Item Based Recommender
-		ItemBasedRecommender recommender = new GenericItemBasedRecommender(dataModel, itemSimilarity);
-		*/
 		
+		 //Item Based CF 
+		 ItemSimilarity itemSimilarity = new LogLikelihoodSimilarity(model);
+		//Create an Item Based Recommender
+		ItemBasedRecommender recommender = new GenericItemBasedRecommender(model, itemSimilarity);
+		
+		/*
 		// User Based CF  
 		UserSimilarity similarity =  null;
 		
@@ -83,22 +111,25 @@ public class RecommendationMain {
 		UserBasedRecommender recommender = new GenericUserBasedRecommender(model, neighborhood, similarity);
 		
 		// 
-		int book_id_to_rec_for = 2;
+		*/
 		int num_recs = 10;
 		List<RecommendedItem> recommendations=null;
-		try {
-			if(!content_based)	
-				recommendations = recommender.recommend(book_id_to_rec_for, num_recs);
-			else recommendations = recommender_item.recommend(book_id_to_rec_for, num_recs);
-		} catch (TasteException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		for(Integer book_id_to_rec_for : book_id_input) {
+			try {
+				if(!content_based)	
+					recommendations = recommender.recommend(book_id_to_rec_for, num_recs);
+				else recommendations = recommender_item.recommend(book_id_to_rec_for, num_recs);
+			} catch (TasteException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println("Recommendation for : "+iDtoTitle.get(book_id_to_rec_for)+" ("+book_id_to_rec_for+")");
+			for (RecommendedItem recommendation : recommendations) {		  
+			  System.out.println(recommendation.getItemID()+": "+iDtoTitle.get((int)recommendation.getItemID()));
+			}
 		}
-		for (RecommendedItem recommendation : recommendations) {
-		  System.out.println(recommendation);
-		}
-		
 		//Evaluations
+		/*
 		RecommenderEvaluator evaluator_map = new AverageAbsoluteDifferenceRecommenderEvaluator();
 		RecommenderEvaluator evaluator_rmse = new RMSRecommenderEvaluator();
 		RecommenderBuilder builder = new MyRecommenderBuilder();
@@ -109,9 +140,11 @@ public class RecommendationMain {
 		} catch (TasteException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
+		} 
+		*
 		System.out.println("RMSE: "+result_rmse);
 		System.out.println("MAP: "+result_map);
+		*/
 		
 	}
 
